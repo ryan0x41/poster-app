edf133cfe88c6b8b338801b9a97e518672bc4b65
Ryan Sheridan <0xryann@protonmail.com>
2025-02-12 23:35:22 +0000

    bug fix

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -16,7 +16,7 @@ kotlin {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
-    
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -24,32 +24,36 @@ kotlin {
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
             baseName = "ComposeApp"
+            freeCompilerArgs += listOf("-Xbinary=bundleId=com.grouph.poster")
             isStatic = true
         }
     }
-    
+
     sourceSets {
-        
-        androidMain.dependencies {
-            implementation(compose.preview)
-            implementation(libs.androidx.activity.compose)
+        androidMain {
+            dependencies {
+                implementation(compose.preview)
+                implementation(libs.androidx.activity.compose)
+            }
         }
-        commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material)
-            implementation(compose.ui)
-            implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
-            implementation(libs.androidx.lifecycle.viewmodel)
-            implementation(libs.androidx.lifecycle.runtime.compose)
+        commonMain {
+            dependencies {
+                implementation(compose.runtime)
+                implementation(compose.foundation)
+                implementation(compose.material)
+                implementation(compose.ui)
+                implementation(compose.components.resources)
+                implementation(compose.components.uiToolingPreview)
+                implementation(libs.androidx.lifecycle.viewmodel)
+                implementation(libs.androidx.lifecycle.runtime.compose)
+            }
         }
     }
 }
 
 android {
+    compileSdk = 35
     namespace = "org.group_h.poster"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "org.group_h.poster"
@@ -75,17 +79,13 @@ android {
 }
 
 dependencies {
-
-
-
     implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.ui.tooling.preview.android)
     implementation(libs.androidx.navigation.runtime.android)
     implementation("androidx.compose.ui:ui:1.5.0")
     implementation("androidx.compose.material3:material3:1.1.0")
-    implementation("androidx.activity:activity-compose:1.7.0")
-    implementation("androidx.navigation:navigation-compose:2.7.0")
-    implementation(libs.androidx.navigation.runtime.android)
+    implementation("androidx.activity:activity-compose:1.10.0")
+    implementation("androidx.navigation:navigation-compose:2.9.0-alpha05")
     debugImplementation("androidx.compose.ui:ui-tooling:1.5.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.22")
     implementation("androidx.core:core-ktx:1.10.0")
--- composeApp/src/androidMain/kotlin/org/group_h/poster/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/group_h/poster/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
